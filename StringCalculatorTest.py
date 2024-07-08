e7154a380c83e5eb20519edda16f9fc1a925f78d
leojones94 <117976870+leojones94@users.noreply.github.com>
2024-07-08 12:27:57 +0530

    Update StringCalculatorTest.py

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -5,20 +5,20 @@ class TestStringCalculator(unittest.TestCase):
         def test_expectZeroForEmptyInput(self):
                 self.assertEqual(add(""), 0)
                 
-        def test_expectZeroForSingleZero(self):
-                self.assertEqual(add("0"), 0)
+        # def test_expectZeroForSingleZero(self):
+        #         self.assertEqual(add("0"), 0)
                 
-        def test_expectSumForTwoNumberst(self):
-                self.assertEqual(add("1,2"), 3)
+        # def test_expectSumForTwoNumberst(self):
+        #         self.assertEqual(add("1,2"), 3)
                 
-        def test_ignoreNumbersGreaterThan1000(self):
-                self.assertEqual(add("1,1001"), 1)
+        # def test_ignoreNumbersGreaterThan1000(self):
+        #         self.assertEqual(add("1,1001"), 1)
                 
-        def test_expectSumWithCustomDelimiter(self):
-                self.assertEqual(add("//;\n1;2"), 3)
+        # def test_expectSumWithCustomDelimiter(self):
+        #         self.assertEqual(add("//;\n1;2"), 3)
                 
-        def test_expectSumWithNewlineDelimiter(self):
-                self.assertEqual(add("1\n2,3"),6);
+        # def test_expectSumWithNewlineDelimiter(self):
+        #         self.assertEqual(add("1\n2,3"),6);
         
 
 
