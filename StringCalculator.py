8122c9b6fe89d7ae5bf621cf662cd36262b71aac
leojones94 <117976870+leojones94@users.noreply.github.com>
2024-07-12 00:19:55 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,20 +1,32 @@
-def two_numb_add(a):
+def numb_add(a):
   sum = 0
   for i in a:
     if int(i) < 1000:
       sum+=int(i)
   return sum
 
+def newline(x):
+  a=[]
+  b = x.split("\n")
+  for j in b:
+    if "," in j:
+      a.append([p for p in (j.split(","))])
+    elif "," not in j:
+      a.append([j])
+  return sum(a,[])
+
 def delimit(x):
   if x[:2] == "//":
     a = x[4:].split(x[2])
     return a
-  a = x.split(",")
-  return a
+  return (newline(x))
+  
   
 def add(x):
   if x=="" or x=="0":
     return 0
   a = delimit(x)
-  sum = two_numb_add(a)
+  sum = numb_add(a)
   return sum
+
+print(add("1\n2,3"))
